820068da1c9f339d379b84e3c43c9228b585e78e
joseph-vincento <joseph.vincento@gmail.com>
2024-02-29 13:49:21 -0500

    added readme

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -22,16 +22,10 @@ void set_up_matrix(vector<vector<int>> &matrix, const string file_name) {
     return;
 }
 
-int main() {
-    vector<vector<int>> zero_matrix;
-    set_up_matrix(zero_matrix, "graph_0.txt");
-
-    vector<vector<int>> one_matrix;
-    set_up_matrix(one_matrix, "graph_1.txt");
-
-    vector<vector<int>> m_one_matrix;
-    set_up_matrix(m_one_matrix, "graph_m1.txt");
-
+// auxillary function to run algorithm as described in lab document
+void compute_expensive_digraph_exact_paths(vector<vector<int>> &zero_matrix,
+                                            vector<vector<int>> &one_matrix,
+                                            vector<vector<int>> &m_one_matrix) {
     // assuming all matricies are square and equal size
     int n = zero_matrix.size();
     for(int l = 2; l <= (3 * n * n + 1); l++) {
@@ -55,6 +49,19 @@ int main() {
             }
         }
     }
+}
+
+int main() {
+    vector<vector<int>> zero_matrix;
+    set_up_matrix(zero_matrix, "graph_0.txt");
+
+    vector<vector<int>> one_matrix;
+    set_up_matrix(one_matrix, "graph_1.txt");
+
+    vector<vector<int>> m_one_matrix;
+    set_up_matrix(m_one_matrix, "graph_m1.txt");
+
+    compute_expensive_digraph_exact_paths(zero_matrix, one_matrix, m_one_matrix);
 
     cout << zero_matrix << endl;
     cout << one_matrix << endl;
